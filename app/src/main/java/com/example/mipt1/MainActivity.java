e80e56556d2f59c9dda760fa3dda53a9b94d60b0
Kremas123 <maka2004@gmail.com>
2024-09-30 16:52:29 +0300

    Pridetas button. kad rodytu hello world

--- app/src/main/java/com/example/mipt1/MainActivity.java
+++ app/src/main/java/com/example/mipt1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mipt1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,22 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // Set window insets padding
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Find the Button and TextView by their IDs
-        Button myButton = findViewById(R.id.button); // Button
-        TextView myTextView = findViewById(R.id.textView); // TextView (initially hidden)
 
-        // Set OnClickListener for the button
-        myButton.setOnClickListener(v -> {
-            // When the button is clicked, show the "Hello World!" text
-            myTextView.setVisibility(View.VISIBLE);
-        });
+        TextView textView = findViewById(R.id.textView);
+        Button showButton = findViewById(R.id.button);
+        Button colorButton = findViewById(R.id.colorButton);
+
+
+        showButton.setOnClickListener(v -> textView.setVisibility(View.VISIBLE));
+
+
+        colorButton.setOnClickListener(v -> textView.setTextColor(Color.RED));
     }
 }
